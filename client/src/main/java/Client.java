a8b28fde937faba9ffab6774867123f4c3cc1400
Mateo Rubio <1089931006@u.icesi.edu.co>
2024-10-27 19:50:08 -0500

    test mode and data for analisis

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class Client {
     public static void main(String[] args) {
@@ -12,28 +15,84 @@ public class Client {
                 throw new Error("Invalid proxy");
             }
 
-            // ExecutorService executor = Executors.newFixedThreadPool(2);
-            boolean coninuity = true;
-            while (coninuity) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Que modo de uso desea?");
+            System.out.println("1. Modo Singular");
+            System.out.println("2. Modo test");
+            System.out.print("Ingrese el número de la opción: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            boolean flag = true;
+            int numero = 0;
+            int count = 0;
 
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Ingrese un número de puntos para enviar al servidor: ");
-                int numero = scanner.nextInt();
-                scanner.nextLine();
-                printer.sendValue(numero); // Método que envía el valor al servidor
-                long startTime = System.currentTimeMillis();
-                double pi = printer.estimatePi(numero);
-                System.out.println("Valor estimado de pi: " + pi);
-                long endTime = System.currentTimeMillis();
-                System.out.println("Tiempo total: " + (endTime - startTime) + "ms");
-                System.out.print("Desea continuar? (s/n): ");
-                String continuar = scanner.nextLine();
-                if (continuar.equals("n")) {
-                    coninuity = false;
-                }
-                if (pi == 0) {
-                    communicator.waitForShutdown();
-                }
+            switch (option){
+                case 1:
+                    while (flag) {
+                        System.out.print("Ingrese un número de puntos para enviar al servidor: ");
+                        numero = scanner.nextInt();
+                        scanner.nextLine();
+                        printer.sendValue(numero); // Método que envía el valor al servidor
+                        long startTime = System.currentTimeMillis();
+                        double pi = printer.estimatePi(numero);
+                        System.out.println("Valor estimado de pi: " + pi);
+                        long endTime = System.currentTimeMillis();
+                        System.out.println("Tiempo total: " + (endTime - startTime) + "ms");
+                        if (pi == 0) {
+                            communicator.waitForShutdown();
+                        }
+                        System.out.println("Desea continuar? (s/n)");
+                        String continuar = scanner.nextLine();
+                        if (!continuar.equals("s")){
+                            flag = false;
+                        }
+                    }
+                    break;
+                case 2:
+                    PrintStream out = new PrintStream(new FileOutputStream("output.txt",true));
+                    PrintStream consoleOut = System.out;
+                    while (flag){
+
+                        System.out.print("Ingrese un número de puntos para enviar al servidor: ");
+                        numero = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.println("Ingrese el numero de cantidad de veces que desea enviar el numero de puntos al servidor: ");
+                        count = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.setOut(out);
+
+                        System.out.println("Begin - " + numero + " points");
+
+                        for (int i = 0; i < count; i++) {
+                            printer.sendValue(numero);
+
+                            long startTime = System.currentTimeMillis();
+                            double pi = printer.estimatePi(numero);
+
+                            System.out.println("Valor estimado de pi: " + pi);
+                            long endTime = System.currentTimeMillis();
+                            System.out.println("Tiempo total: " + (endTime - startTime) + "ms");
+
+                            if (pi == 0) {
+                                communicator.waitForShutdown();
+                            }
+                        }
+                        System.out.println();
+
+                        System.setOut(consoleOut);
+
+                        System.out.println("Desea continuar? (s/n)");
+                        String continuar = scanner.nextLine();
+                        if (!continuar.equals("s")){
+                            flag = false;
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    return;
             }
 
             communicator.close();
